ca8fd935501a8de9eee0d03194333d9090b56dca
eldyblue <eldera@live.fr>
2025-03-01 05:57:30 +0100

    try removing alpine

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import Alpine from 'alpinejs'
+// import Alpine from '/node_modules/alpinejs/src/alpinejs'
  
-window.Alpine = Alpine
+// window.Alpine = Alpine
  
-Alpine.start()
+// Alpine.start()
